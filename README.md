aa69d75fe2185313f1f206e1e5d64f8bce0a1885
An0ther0ne <58149473+An0ther0ne@users.noreply.github.com>
2020-01-21 00:14:20 +0200

    Serpinski Triangle

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Fractals
 Few experiments with fractals and Python...
+
+## Serpinski Randomised Triangle
+
+![Serpinski Randomised Triangle](img/Serpinski3.png)
+
+## Requirements:
+
+* Python
+* NumPy
+* MatPlotLib
+
+## AUTHOR
+   An0ther0ne
---
+++ fractals.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+# Serpinski Triangle with random and matplotlib
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use(['ggplot','seaborn-talk'])
+plt.gcf().canvas.set_window_title('Fractals demo with random. Press [ESC] to Exit...')
+
+# --- Globals
+plot_width = 500
+plot_height = plot_width
+CurrentIteration = 0
+IterationsPerFrame = 100
+MaxIterations = 10000
+Show = True
+
+# Randomly plot first pixel
+x0 = np.random.randint(plot_width)
+y0 = np.random.randint(plot_height)
+
+def onkeypressed(event):			
+	global Show
+	if event.key == 'escape':
+		Show = False
+
+class Point2D:
+	def __init__(self, p2d):
+		self.x = p2d[0]
+		self.y = p2d[1]
+		
+class Triangle:		
+	def __init__(self):
+		self.p1 = Point2D((0, 0))
+		self.p2 = Point2D((plot_width, 0))
+		self.p3 = Point2D((x0, plot_width*np.sqrt(3)//2))
+		self.points = [self.p1, self.p2, self.p3]
+
+triangle = Triangle()
+
+xticks = np.zeros(IterationsPerFrame)
+yticks = np.zeros(IterationsPerFrame)
+plt.yticks([])
+plt.xticks([]) 
+
+while Show and CurrentIteration < MaxIterations:
+	points = np.random.randint(0,3,IterationsPerFrame)
+	for i,p in enumerate(points):
+		xticks[i] = (x0 + triangle.points[p].x) // 2
+		yticks[i] = (y0 + triangle.points[p].y) // 2
+		x0, y0 = xticks[i], yticks[i]
+	CurrentIteration += IterationsPerFrame
+	plt.plot(xticks, yticks, 'r.')
+	plt.title("Serpinski randomised Triangle. Ineretions: {}".format(CurrentIteration))
+	plt.connect('key_press_event', onkeypressed)
+	plt.pause(0.01)
+if 	CurrentIteration >= MaxIterations:
+	plt.show()
