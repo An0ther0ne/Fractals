9d2db00856cfa1e987d0afbe9689367ec51f4418
An0ther0ne <58149473+An0ther0ne@users.noreply.github.com>
2020-01-21 13:12:57 +0200

    add rectangle

--- README.md
+++ README.md
@@ -3,12 +3,22 @@ Few experiments with fractals and Python...
 
 ## Serpinski Randomised Triangle
 
-![Serpinski Randomised Triangle fig 1](img/Serpinski31.png)
+![Serpinski Randomised Triangle fig 1](img/Serpinski3.png)
 You can zoom In and Out of this figure.
-For example zoom in:  
-![Serpinski Randomised Triangle fig 2](img/Serpinski32.png)
-And again:
-![Serpinski Randomised Triangle fig 3](img/Serpinski33.png)
+
+### Algorithm
+
+1. Start with equilateral triangle and number its vertices 1,2,3
+2. Plot a random dot point
+3. Calculate a random integer number in range from 1 to number of triangle vertices (3)
+4. Find a middle point from a random point (see para.2) to triangle vertice with number, calculated in para. 3, and plot a new dot point at them.
+5. Store coordinates of dot point as new random instead of para 2.
+6. Repeat steps 3-5 as many times as you wish. 
+
+## Serpinski Randomised Rectangle
+
+In the next figure shown rectangle, filled with similar chaos algorithm.
+![Serpinski Randomised Triangle fig 2](img/Serpinski41.png)
 
 ## Requirements:
 
--- fractals.py
+++ fractals.py
@@ -5,19 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.style.use(['ggplot','seaborn-talk'])
-plt.gcf().canvas.set_window_title('Fractals demo with random. Press [ESC] to Exit...')
+plt.gcf().canvas.set_window_title('Fractals chaos method demo. Press [ESC] to the next figure...')
 
 # --- Globals
-plot_width = 500
+plot_width = 65536
 plot_height = plot_width
+IterationsPerFrame = 4096
+MaxIterations = 65536
 CurrentIteration = 0
-IterationsPerFrame = 1000
-MaxIterations = 20000
 Show = True
-
-# Randomly plot first pixel
-x0 = np.random.randint(plot_width)
-y0 = np.random.randint(plot_height)
 
 def onkeypressed(event):			
 	global Show
@@ -33,18 +29,36 @@ class Triangle:
 	def __init__(self):
 		self.p1 = Point2D((0, 0))
 		self.p2 = Point2D((plot_width, 0))
-		self.p3 = Point2D((x0, plot_width*np.sqrt(3)//2))
+		self.p3 = Point2D((plot_width // 2, plot_width*np.sqrt(3)//2))
 		self.points = [self.p1, self.p2, self.p3]
+	
+class Rectangle:
+	def __init__(self, base):
+		self.p1 = Point2D((0,0))
+		self.p2 = Point2D((base,0))
+		self.p3 = Point2D((base,base))
+		self.p4 = Point2D((0,base))
+		self.points = [self.p1, self.p2, self.p3, self.p4]
+		
+triangle  = Triangle()
+rectangle = Rectangle(plot_width)
 
-triangle = Triangle()
+for p in rectangle.points:
+	print("x={}, y={}".format(p.x,p.y))
+exit
 
 xticks = np.zeros(IterationsPerFrame)
 yticks = np.zeros(IterationsPerFrame)
 plt.yticks([])
 plt.xticks([]) 
 
+# --- Serpinski randomised Triangle
+plt.subplots_adjust(bottom=0.01, top=0.96, left=0.01, right=0.99)
+# Randomly plot first pixel
+x0 = np.random.randint(plot_width)
+y0 = np.random.randint(plot_height)
 while Show and CurrentIteration < MaxIterations:
-	points = np.random.randint(0,3,IterationsPerFrame)
+	points = np.random.randint(0, 3, IterationsPerFrame)
 	for i,p in enumerate(points):
 		xticks[i] = (x0 + triangle.points[p].x) // 2
 		yticks[i] = (y0 + triangle.points[p].y) // 2
@@ -54,7 +68,36 @@ while Show and CurrentIteration < MaxIterations:
 	plt.title("Serpinski randomised Triangle. Iterations: {}".format(CurrentIteration))
 	plt.connect('key_press_event', onkeypressed)
 	plt.pause(0.01)
-	
-plt.gcf().canvas.set_window_title('Fractal Paint finished. Just close this window.')	
-if 	CurrentIteration >= MaxIterations:
-	plt.show()
+plt.gcf().canvas.set_window_title('Fractal paint finished. Press [ESC] to the next one.')
+Show = True	
+while Show:
+	plt.connect('key_press_event', onkeypressed)
+	plt.pause(0.1)
+plt.cla()
+plt.gcf().canvas.set_window_title('Fractals chaos method demo. Press [ESC] to the next one...')
+plt.style.use(['ggplot','seaborn-talk'])
+plt.subplots_adjust(bottom=0.01, top=0.96, left=0.01, right=0.99)
+plt.yticks([])
+plt.xticks([]) 
+
+# --- Serpinski randomised Rectangle
+CurrentIteration = 0
+Show = True
+# Randomly plot first pixel
+x0 = np.random.randint(plot_width)
+y0 = np.random.randint(plot_height)
+while Show and CurrentIteration < MaxIterations:
+	points = np.random.randint(0, 4, IterationsPerFrame)
+	for i,p in enumerate(points):
+		xticks[i] = (x0 + rectangle.points[p].x) // 3
+		yticks[i] = (y0 + rectangle.points[p].y) // 3
+		x0, y0 = xticks[i], yticks[i]
+	CurrentIteration += IterationsPerFrame
+	plt.plot(xticks, yticks, 'r.')
+	plt.title("Serpinski randomised Rectangle. Iterations: {}".format(CurrentIteration))
+	plt.connect('key_press_event', onkeypressed)
+	plt.pause(0.01)
+plt.gcf().canvas.set_window_title('Fractal paint finished. Just close this window.')
+plt.show()
+
+
